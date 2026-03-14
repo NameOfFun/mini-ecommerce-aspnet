e28ac1c58f5e780e062e9dde3a6e0c463198ce43
NameOfFun <12damhao@gmail.com>
2026-03-14 23:57:51 +0700

    fig bug

--- Models/Product.cs
+++ Models/Product.cs
@@ -31,7 +31,7 @@ public partial class Product
     public string SupplierId { get; set; } = null!;
     [JsonIgnore]
     public virtual Category Category { get; set; } = null!;
-
+    [JsonIgnore]
     public virtual ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
 
     public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
